613218103ab88b8fd92ddecc4f3c42d79c0a85b8
SpareAByte <104662326+SpareAByte@users.noreply.github.com>
2024-12-11 00:17:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ print(f'{ascii_banner}\nSpareAByte\n\n')
 
 url = input('Enter url: ')
 
-user_wait = input('''
-                  How long between requests?
-                  (Choose an option)
-                  1 : 0s Let the site block me
-                  2 : 1s Play it safe
-                  3 : 1s-3s random
-                  4 : 3s Play it safe
-                  ''')
+#user_wait = input('''
+#                  How long between requests?
+#                  (Choose an option)
+#                  1 : 0s Let the site block me
+#                  2 : 1s Play it safe
+#                  3 : 1s-3s random
+#                  4 : 3s Play it safe.
+#                  ''')
 
 
 formated_url = url_funcs.verify_url(url)
-user_timer = url_funcs.user_timer(user_wait)
+#user_timer = url_funcs.user_timer(user_wait)
 url_scraper.scraper(formated_url)
-print(', '.join(url_scraper.email_list), url_funcs.timer)
-print(user_timer)
+print(url_scraper.email_list)
